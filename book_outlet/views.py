d39559e60ba3c2076c736753817d61ed21dc384c
unknown <karimsalahuddin03@gmaqil.com>
2023-12-20 22:10:51 +0600

    rendering books details stored in database

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
 # Create your views here.
 def index(request):
-    books = Book.objects.all()
-    return render(request, "book_outlet/index.html", {
-        "books" : books 
-    })
+    books = Book.objects.all().order_by("rating")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
     
-def book_detail(request, id):
-    book = get_object_or_404(Book, pk=id)
+    return render(request, "book_outlet/index.html", {
+        "books" : books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating
+        })
+    
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
